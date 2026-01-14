27e96b751bb175636a87875074eefa8b6628796e
Aryel Bargas <aryelbargas@proton.me>
2026-01-14 17:41:05 -0300

    feat: hero page

---
+++ weirdo-petsitter/src/app/_components/hero.tsx
@@ -0,0 +1,19 @@
+export function Hero(){
+    return (
+        <section className="bg-linear-to-b from-sky-500 to-indigo-500 text-gray-50 w-full overflow-hidden">
+            <div>
+                <article>
+                    <div>
+                        <h1>Petsitter para Exóticos</h1>
+                        <h4>Especialistas em cuidado e atenção para aracnídeos, répteis, roedores e outros pets fora do comum.</h4>
+                    </div>
+                    <div>
+                        <p>Especialistas em animais exóticos certificados</p>
+                        <p>Controle rigoroso de habitat e temperatura</p>
+                        <p>Dietas específicas e manejo adequado</p>
+                    </div>
+                </article>
+            </div>
+        </section>
+    )
+}
